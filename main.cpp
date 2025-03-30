7224fcbedfd1f2a7baeced2e54a74b12ad7ccff7
Rbooii <arconumberone@gmail.com>
2025-03-30 14:25:45 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Helpers
+double power(double x, int pow) {
+    double result = 1.0;
+    for (int i = 1; i <= pow; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+int factorial(int x) {
+    if (x == 0 || x == 1) return 1;
+    int result = x;
+    for (int i = x - 1; i > 1; i--) {
+        result *= i;
+    }
+    return result;
+}
+
+int combinate(int n, int k) {
+    return factorial(n) / (factorial(n - k) * factorial(k));
+}
+
+double binomial(int n, int k, double success, double fail) {
+    return combinate(n, k) * power(success, k) * power(fail, n - k);
+}
+/* 
+n = how many tries
+k = how much success you want
+success = probability of success 1 = 100%, 0 = 0% use decimal base 2
+fail = probability of fail 1 = 100%, 0 = 0% use decimal base 2
+Note : fail + success MUST HAVE TO BE 1
+*/
+
+int main() {
+    cout << binomial(10, 2, 0.5, 0.5)*100 << endl; 
+    return 0;
+}
